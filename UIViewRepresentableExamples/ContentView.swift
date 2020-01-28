f25d061f12390bf279d790872fe29a2b121cd296
Bart van Kuik <bart@vankuik.nl>
2020-01-28 09:32:58 +0100

    Added empty project

---
+++ UIViewRepresentableExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIViewRepresentableExamples
+//
+//  Created by bartvk on 28/01/2020.
+//  Copyright © 2020 DutchVirtual. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
