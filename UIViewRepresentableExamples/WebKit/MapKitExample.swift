2bea24c629c0943bc3733e31318b19bd9f626269
Bart van Kuik <bart@vankuik.nl>
2020-01-29 19:44:30 +0100

    Current MapKit example gives the error: "Modifying state during view update, this will cause undefined behavior"

--- UIViewRepresentableExamples/WebKit/MapKitExample.swift
+++ UIViewRepresentableExamples/WebKit/MapKitExample.swift
@@ -1,9 +1,27 @@
 import SwiftUI
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    var description: String {
+        String(format: "%.8f, %.8f", self.latitude, self.longitude)
+    }
+}
 
 struct MapKitExample: View {
+    static let amsterdam = CLLocationCoordinate2D(latitude: 52.37403,
+                                                  longitude: 4.88969)
+    @State private var centerCoordinate = Self.amsterdam
+    @State var resetCoordinate:  CLLocationCoordinate2D? = nil
+
     var body: some View {
-        MapView()
-            .navigationBarTitle("MapKit Example")
+        VStack {
+            MapView(centerCoordinate: self.$centerCoordinate, resetCoordinate: self.$resetCoordinate)
+            Text("Centered on: " + self.centerCoordinate.description)
+            Button("Reset") {
+                self.resetCoordinate = Self.amsterdam
+            }
+        }
+        .navigationBarTitle("MapKit Example")
     }
 }
 
--- UIViewRepresentableExamples/WebKit/MapView.swift
+++ UIViewRepresentableExamples/WebKit/MapView.swift
@@ -2,20 +2,35 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    @Binding var resetCoordinate:  CLLocationCoordinate2D?
+
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
         return mapView
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
+        if let newCoordinate = self.resetCoordinate {
+            uiView.centerCoordinate = newCoordinate
+        }
     }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, MKMapViewDelegate {
         var parent: MapView
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            if parent.centerCoordinate.latitude != mapView.centerCoordinate.latitude &&
+                parent.centerCoordinate.longitude != mapView.centerCoordinate.longitude {
+                
+                parent.centerCoordinate = mapView.centerCoordinate
+            }
+        }
         
         init(_ parent: MapView) {
             self.parent = parent
@@ -24,7 +39,9 @@ struct MapView: UIViewRepresentable {
 }
 
 struct MapView_Previews: PreviewProvider {
+    static let amsterdamCoordinate = CLLocationCoordinate2D(latitude: 52.37403, longitude: 4.88969)
+    
     static var previews: some View {
-        MapView()
+        MapView(centerCoordinate: .constant(amsterdamCoordinate), resetCoordinate: .constant(nil))
     }
 }
