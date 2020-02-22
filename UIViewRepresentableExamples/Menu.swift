c0f59cf4be4d0209d96220e5ee73a05cb6330808
Bart van Kuik <bart@vankuik.nl>
2020-02-22 09:41:45 +0100

    Removed GeometryReader from main view

--- UIViewRepresentableExamples/Menu.swift
+++ UIViewRepresentableExamples/Menu.swift
@@ -1,42 +1,41 @@
 import SwiftUI
 
+struct PadMessage: View {
+    var body: some View {
+        Color.clear.overlay (
+            GeometryReader { geometry in
+                Text(geometry.size.width < geometry.size.height
+                    ? "Rotate to see the list of examples"
+                    : "No example selected"
+                ).font(.headline).foregroundColor(.gray)
+            }
+        )
+    }
+}
+
 struct Menu: View {
     var body: some View {
-        GeometryReader { geometry in
-            NavigationView {
-                List {
-                    NavigationLink(destination: HTMLRenderingWebViewExample()) {
-                        Text("HTML rendering WebView")
-                    }
-                    NavigationLink(destination: MapKitExample()) {
-                        Text("MapKit example")
-                    }
-                    NavigationLink(destination: TextViewExample()) {
-                        Text("TextView example")
-                    }
-                    NavigationLink(destination: LegacyScrollViewExample()) {
-                        Text("LegacyScrollView example")
-                    }
-                    NavigationLink(destination: SVGViewExample()) {
-                        Text("SVGView Example")
-                    }
+        NavigationView {
+            List {
+                NavigationLink(destination: HTMLRenderingWebViewExample()) {
+                    Text("HTML rendering WebView")
                 }
-                .navigationBarTitle("Available examples")
-                
-                Text(self.iPadMessage(for: geometry.size)).font(.headline).foregroundColor(.gray)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(GeometryReader { reader in
-                        Color.clear
-                    })
+                NavigationLink(destination: MapKitExample()) {
+                    Text("MapKit example")
+                }
+                NavigationLink(destination: TextViewExample()) {
+                    Text("TextView example")
+                }
+                NavigationLink(destination: LegacyScrollViewExample()) {
+                    Text("LegacyScrollView example")
+                }
+                NavigationLink(destination: SVGViewExample()) {
+                    Text("SVG Example")
+                }
             }
-        }
-    }
-    
-    private func iPadMessage(for size: CGSize) -> String {
-        if size.width < size.height {
-            return "Rotate to see the list of examples"
-        } else {
-            return "No example selected"
+            .navigationBarTitle("Available examples")
+
+            PadMessage()
         }
     }
 }
