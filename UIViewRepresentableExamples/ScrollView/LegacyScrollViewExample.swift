04ba6d7f6ff201563027276568682988e91ca3c4
Bart van Kuik <bart@vankuik.nl>
2020-10-04 19:04:02 +0200

    Edited ScrollView example such that the contents are updated

--- UIViewRepresentableExamples/ScrollView/LegacyScrollViewExample.swift
+++ UIViewRepresentableExamples/ScrollView/LegacyScrollViewExample.swift
@@ -1,18 +1,27 @@
 import SwiftUI
 
+struct ScrollViewContent: View {
+    @Binding var sampleText: String
+    
+    var body: some View {
+        Text(self.sampleText)
+    }
+}
+
 struct LegacyScrollViewExample: View {
     @State private var action = LegacyScrollView.Action.idle
+    @State private var sampleText = Constants.lipsum[0]
     
     var body: some View {
         VStack(spacing: 0) {
             LegacyScrollView(axis: .vertical, action: self.$action) {
-                Text(Constants.lipsum.joined(separator: "\n\n"))
+                ScrollViewContent(sampleText: self.$sampleText)
             }
             .padding(20)
             .background(Color.gray)
             Spacer()
             Button("Set offset") {
-                self.action = LegacyScrollView.Action.offset(x: 0, y: 200, animated: true)
+                self.sampleText += Constants.lipsum[0]
             }.padding()
         }.navigationBarTitle("ScrollView")
     }
