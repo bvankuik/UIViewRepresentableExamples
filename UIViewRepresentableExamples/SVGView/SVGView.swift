e3d217ab29fd26bb091df6e0da20d30fa417aca2
Bart van Kuik <bart.van-kuik@klm.com>
2022-12-27 10:51:54 +0100

    Replaced flag and filter with a single parameter

--- UIViewRepresentableExamples/SVGView/SVGView.swift
+++ UIViewRepresentableExamples/SVGView/SVGView.swift
@@ -2,8 +2,9 @@ import SwiftUI
 import WebKit
 
 struct SVGView: UIViewRepresentable {
+    @Environment(\.colorScheme) private var colorScheme
     @Binding private var svgString: String
-    private let reverseGrayscale: Bool
+    private let darkmodeStyle: String
     private let style =
         """
         html, body { margin:0; padding:0; overflow:hidden }
@@ -13,13 +14,7 @@ struct SVGView: UIViewRepresentable {
         svg { position:fixed; top:0; left:0; height:100%; width:100%;
         }
         """
-    private let reverseGrayscaleFilterStyle =
-        """
-        svg {
-            filter: grayscale(100%) invert(100%) brightness(200%);
-        }
-        """
-    
+
     private let webView: WKWebView
     
     func makeUIView(context: Context) -> WKWebView {
@@ -29,11 +24,11 @@ struct SVGView: UIViewRepresentable {
     func updateUIView(_ uiView: WKWebView, context: Context) {
         if self.svgString != context.coordinator.lastLoadedString {
             context.coordinator.lastLoadedString = self.svgString
-            
+
             let htmlString =
             "<html><head><style>" +
             self.style +
-            (self.reverseGrayscale ? self.reverseGrayscaleFilterStyle : "") +
+            (self.colorScheme == .dark ? self.darkmodeStyle : "" ) +
             "</style>" +
             "<meta name=\"viewport\" content=\"initial-scale=1.0\"/>" +
             "</head><body>" +
@@ -56,7 +51,7 @@ struct SVGView: UIViewRepresentable {
         }
     }
     
-    init(svgString: Binding<String>, isReverseGrayscaleFiltered: Bool = false) {
+    init(svgString: Binding<String>, darkmodeStyle: String = "") {
         let preferences = WKPreferences()
         preferences.javaScriptEnabled = false // JavaScript is not needed
         let config = WKWebViewConfiguration()
@@ -69,16 +64,22 @@ struct SVGView: UIViewRepresentable {
         webView.isUserInteractionEnabled = false // to disable pinch gesture and scrolling
         self.webView = webView
         self._svgString = svgString
-        self.reverseGrayscale = isReverseGrayscaleFiltered
+        self.darkmodeStyle = darkmodeStyle
     }
 }
 
 struct SVGView_Previews: PreviewProvider {
+    static let darkmodeStyle =
+    """
+    svg {
+        filter: grayscale(100%) invert(100%) brightness(200%);
+    }
+    """
     static var previews: some View {
         ForEach(ColorScheme.allCases, id: \.self) {
             SVGView(
                 svgString: .constant(SVGExamples.stars),
-                isReverseGrayscaleFiltered: ($0 == .dark)
+                darkmodeStyle: Self.darkmodeStyle
             )
             .preferredColorScheme($0)
         }
